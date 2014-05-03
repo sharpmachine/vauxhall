624c6dc363b427b01687fc87657ddb48685f3b7a
Jesse Kade <jessekade@gmail.com>
2014-05-02 17:27:16 -0700

    stuff

---
+++ local-config.SAMPLE.php
@@ -0,0 +1,10 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the four main database defines
+*/
+
+define( 'DB_NAME', 'DATABASE NAME' );
+define( 'DB_USER', 'DATABASE USER' );
+define( 'DB_PASSWORD', 'DATABASE PASSWORD' );
+define( 'DB_HOST', 'DATABASE HOST' ); // Probably 'localhost'
